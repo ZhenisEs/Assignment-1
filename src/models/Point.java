c7356f1196c05aa49b970f19aa9906920cf954fd
esimb <230723@astanait.edu.kz>
2023-12-22 20:50:53 +0300

    second

---
+++ src/models/Point.java
@@ -0,0 +1,18 @@
+package models;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+    public double distanceTo(Point another){
+        double distance = Math.sqrt(Math.pow((another.x - this.x), 2) + Math.pow((another.y - this.y), 2));
+        return distance;
+    }
+}
+
+
+
---
+++ src/models/Shape.java
@@ -0,0 +1,45 @@
+package models;
+import java.util.ArrayList;
+
+public class Shape {
+    private ArrayList<Point> pointList = new ArrayList<>();
+
+    public void addPoint(Point point){
+        pointList.add(point);
+    }
+
+    public double calculatePerimeter(){
+        double perimeter = 0.0;
+        int numPoints = pointList.size();
+
+        for (int i = 0; i < numPoints; i++){
+            int nextIndex = (i+1) % numPoints;
+
+            perimeter += pointList.get(i).distanceTo(pointList.get(nextIndex));
+        }
+            return perimeter;
+
+
+    }
+
+    public double getLongest(){
+        double max_side = 0;
+        for (int i = 0; i < pointList.size(); i++){
+            for (int k = i+1; k < pointList.size(); k++){
+                double distance = pointList.get(i).distanceTo(pointList.get(k));
+                if(distance>max_side){
+                    max_side = distance;
+                }
+
+            }
+        }
+        return max_side;
+    }
+
+    public double averageSide(){
+        double perimeter = calculatePerimeter();
+        double average = perimeter / pointList.size();
+        return average;
+    }
+
+}
